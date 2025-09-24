c14c6537374de03a5200cc1b3e88c81bbf1a6126
MileshPatel28 <milesh.patel28@gmail.com>
2025-09-24 15:17:11 -0400

    Song display work

--- app/app/playlist.tsx
+++ app/app/playlist.tsx
@@ -1,14 +1,63 @@
 import { ThemedText } from "@/components/themed/themed-text";
 import { ThemedView } from "@/components/themed/themed-view";
 import Collapsible from "react-native-collapsible";
+import Accordion from 'react-native-collapsible/Accordion';
 
 
 
-
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import { theme } from "@/components/theme";
+import TrackPlayer from "react-native-track-player";
 
 export default function Playlists(){
-    const [isCollapsed, setIsCollapsed] = useState(false);
+    const [activeSections, setActiveSections] = useState<number[]>([]);
+    const [tracks, setTracks] = useState<any[]>([]);
+
+    useEffect(() => {
+        async function loadTracks() {
+            setTracks(await TrackPlayer.getQueue())
+        }
+        
+        loadTracks();
+    }, []);
+
+
+    function createAllSongList() {
+        return (
+            <ThemedView
+                style={{
+                    marginVertical: 10,
+                    padding: 10
+                }}
+            >
+                {tracks.map((track, index) => (
+                    singleSongTile(track)
+                ))}
+            </ThemedView>
+        );
+    }
+
+    function singleSongTile(singleTrack:any) {
+        return(
+            <ThemedView 
+                style={{
+                    borderColor: theme.colors.outline,
+                    borderWidth: 2,
+                    borderCurve: 'circular',
+                    borderRadius: 10,
+                    padding: 3,
+                    margin: 5,
+                }}>
+                <ThemedText
+                    style={{
+                        fontSize: 20,
+                    }}
+                > {singleTrack.title} </ThemedText>
+            </ThemedView>
+        )
+    }
+
 
     return(
         <ThemedView
@@ -16,11 +65,34 @@ export default function Playlists(){
                 flex: 1,
                 padding: 50
             }}>
-            <ThemedText>
-                Playlists!
-            </ThemedText>
 
+            <Accordion
+                sections={[{ title: "All Songs", content: "" }]}
+                activeSections={activeSections}
+                renderHeader={(section, _) => (
+                    <ThemedView 
+                        style={{
+                            flexDirection: 'row',
+                            alignItems: 'center'
+                        }}> 
+                        <MaterialIcons name="library-music" size={40} color={theme.colors.text} />
+                        <ThemedText style={{ 
+                                fontWeight: "bold",
+                                fontSize: 28,
+                                marginHorizontal: 8
+                            }}>
+                            {section.title}
+                        </ThemedText>
+                    </ThemedView>
 
+                )}
+                renderContent={(section) => (
+                    createAllSongList()
+                )}
+                onChange={setActiveSections}
+            />
+
+            
 
         </ThemedView>
     )
