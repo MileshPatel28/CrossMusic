0aa132ec4e2cf965bde8dfbb71d54a9682cc7648
MileshPatel28 <milesh.patel28@gmail.com>
2025-11-07 16:06:46 -0500

    Single song plays in android app

--- android/app/src/main/java/com/github/mileshpatel28/crossmusic/MainActivity.kt
+++ android/app/src/main/java/com/github/mileshpatel28/crossmusic/MainActivity.kt
@@ -3,12 +3,14 @@ package com.github.mileshpatel28.crossmusic
 import android.media.AudioAttributes
 import android.media.MediaPlayer
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
-import androidx.activity.enableEdgeToEdge
+import android.widget.SeekBar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.github.mileshpatel28.crossmusic.databinding.ActivityMainBinding
+import kotlin.math.floor
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,17 +19,33 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityMainBinding
 
+    val mediaPlayer = MediaPlayer()
+    val handler: Handler = Handler(Looper.getMainLooper())
+    private val updateSeekBar : Runnable =  Runnable {
+
+        run {
+            if(mediaPlayer.isPlaying()){
+                binding.tvSongTime.setText(formatDuration(mediaPlayer.currentPosition.toDouble()/1000))
+                binding.tvSongTimeInverse.setText("-${formatDuration((mediaPlayer.duration - mediaPlayer.currentPosition).toDouble()/1000)}")
+            }
+
+            handler.postDelayed(updateSeekBar,500)
+        }
+
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
         binding = ActivityMainBinding.inflate(layoutInflater);
+        setContentView(binding.root)
 
-        val url = "http://${getString(R.string.server_adresse)}:3001/songs/Blank.mp3"
 
-        Log.v("DEBUG", url);
+        val url = "http://${getString(R.string.server_adresse)}:3002/songs/Blank.mp3"
 
-        val mediaPlayer = MediaPlayer().apply {
+
+        mediaPlayer.apply {
             setAudioAttributes(
                 AudioAttributes.Builder()
                     .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
@@ -35,12 +53,30 @@ class MainActivity : AppCompatActivity() {
                     .build()
             )
             setDataSource(url)
-            prepare()
+            prepareAsync()
         }
 
 
-        binding.btnPlayPause.setOnClickListener {
 
+
+
+
+        mediaPlayer.setOnPreparedListener {
+            Log.v("DEBUG", " listener ready")
+            binding.tvSongTime.setText(formatDuration(mediaPlayer.currentPosition.toDouble()/1000))
+            binding.tvSongTimeInverse.setText("-${formatDuration((mediaPlayer.duration - mediaPlayer.currentPosition).toDouble()/1000)}")
+            binding.seekBar.max = mediaPlayer.duration
+            handler.post(updateSeekBar)
+        }
+
+        binding.seekBar.setOnClickListener {
+            Log.v("DEBUG", binding.seekBar.progress.toString())
+        }
+        binding.seekBar.setOnSeekBarChangeListener(SeekBarListener())
+
+
+
+        binding.btnPlayPause.setOnClickListener {
             if(isPlaying){
                 isPlaying = false;
                 mediaPlayer.pause()
@@ -51,6 +87,9 @@ class MainActivity : AppCompatActivity() {
                 mediaPlayer.start()
                 binding.btnPlayPause.setImageResource(R.drawable.pause);
             }
+
+            binding.tvSongTime.text = formatDuration(mediaPlayer.currentPosition.toDouble()/1000)
+
         }
 
 
@@ -62,4 +101,45 @@ class MainActivity : AppCompatActivity() {
 
 
     }
+
+
+    fun formatDuration(value : Double) : String{
+        val hrs = floor(value / 3600)
+        val mins = floor((value % 3600) / 60)
+        val secs = floor(value % 60)
+
+
+        if(hrs > 0){
+            return "${hrs.toInt()}:${mins.toInt().toString().padStart(2,'0')}:${secs.toInt().toString().padStart(2, '0')}"
+        }
+        else{
+            return "${mins.toInt()}:${secs.toInt().toString().padStart(2, '0')}";
+        }
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        handler.removeCallbacks(updateSeekBar)
+    }
+
+
+    inner class SeekBarListener : SeekBar.OnSeekBarChangeListener{
+        override fun onProgressChanged(
+            seekBar: SeekBar?,
+            progress: Int,
+            fromUser: Boolean
+        ) {
+            mediaPlayer.seekTo(progress)
+            binding.tvSongTime.setText(formatDuration(mediaPlayer.currentPosition.toDouble()/1000))
+            binding.tvSongTimeInverse.setText("-${formatDuration((mediaPlayer.duration - mediaPlayer.currentPosition).toDouble()/1000)}")
+        }
+
+        override fun onStartTrackingTouch(p0: SeekBar?) {}
+
+        override fun onStopTrackingTouch(p0: SeekBar?) {}
+
+    }
+
+
 }
--- android/app/src/main/java/com/github/mileshpatel28/crossmusic/SeekBarListener.kt
+++ android/app/src/main/java/com/github/mileshpatel28/crossmusic/SeekBarListener.kt
@@ -1,4 +1,17 @@
 package com.github.mileshpatel28.crossmusic
 
-class SeekBarListener {
+import android.widget.SeekBar
+
+class SeekBarListener : SeekBar.OnSeekBarChangeListener {
+    override fun onProgressChanged(
+        p0: SeekBar?,
+        p1: Int,
+        p2: Boolean
+    ) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onStartTrackingTouch(p0: SeekBar?) {}
+
+    override fun onStopTrackingTouch(p0: SeekBar?) {}
 }
